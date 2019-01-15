3392477753f23bb9974752fe48745c3f9a0a62e7
tztztztztz <120165967@qq.com>
2019-01-15 22:41:38 +0800

    fix bug

--- util/bbox.py
+++ util/bbox.py
@@ -41,8 +41,8 @@ def box_ious(box1, box2):
 
     inter = iw * ih
 
-    box1_area = (box1[:, 2] - box1[:, 0] + 1) * (box1[:, 3] - box1[:, 1] + 1)
-    box2_area = (box2[:, 2] - box2[:, 0] + 1) * (box2[:, 3] - box2[:, 1] + 1)
+    box1_area = (box1[:, 2] - box1[:, 0]) * (box1[:, 3] - box1[:, 1])
+    box2_area = (box2[:, 2] - box2[:, 0]) * (box2[:, 3] - box2[:, 1])
 
     box1_area = box1_area.view(N, 1)
     box2_area = box2_area.view(1, K)
